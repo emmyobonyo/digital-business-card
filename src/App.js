4dd68bac20f4987b956dd55094d66c2fa8b0820d
emmyobonyo@gmail.com <emmyobonyo@gmail.com>
2022-02-06 14:38:48 +0300

    Add an about section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header';
+import About from './components/about/About';
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <About />
     </div>
   );
 }
